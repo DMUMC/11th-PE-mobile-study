dc23a74227d38509aff672edc26a5d4f2800c958
JeongGyul <jjascd@naver.com>
2026-09-11 15:51:43 +0900

    feat: for문을 이용한 영화 제목 출력

---
+++ JeongGyul/movielog/lib/dart_practice.dart
@@ -0,0 +1,18 @@
+void main() {
+  const movies= <Movie>[
+    Movie(id: 1, title: "오디세이"),
+    Movie(id: 2, title: "스파이더맨"),
+    Movie(id: 3, title: "호프")
+  ];
+
+  for(final movie in movies) {
+    print(movie.title);
+  }
+}
+
+class Movie {
+  const Movie({required this.id, required this.title});
+
+  final int id;
+  final String title;
+}
